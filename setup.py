9164f13dc078d326c1d0d2261b4f13148b5c2fe2
Alexander DeRieux <alexander.derieux@gmail.com>
2021-11-08 12:33:30 -0500

    New package setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Installation script for SmartStock.
+"""
+import os
+from setuptools import setup
+
+# Path to current directory.
+CWD = os.path.abspath(os.path.dirname(__file__))
+
+def get_install_requires():
+
+    # Parse dependencies from requirements file.
+    # This is useful for Docker-based installs.
+    with open(os.path.join(CWD, 'requirements.txt')) as f:
+        return f.read().splitlines()
+
+def get_description():
+
+    # Obtain long description from README file.
+    with open(os.path.join(CWD, 'README.md')) as f:
+        return f.read()
+
+setup(
+    name='smart-stock',
+    version='0.0.1',
+    description='SmartStock Reinforcement Learning',
+    long_description=get_description(),
+    install_requires=get_install_requires(),
+    author='Alexander DeRieux',
+    author_email='alexander.derieux@gmail.com',
+    url='https://github.com/zanderman/smart-stock-rl',
+)
