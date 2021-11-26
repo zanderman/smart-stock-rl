c1992d365cb00fef227cbc846fa0aacaa68e2ea6
Alexander DeRieux <alexander.derieux@gmail.com>
2021-11-26 12:23:57 -0500

    New namedtuple for state-action transitions in replay memory

--- smart_stock/algorithms/dqn.py
+++ smart_stock/algorithms/dqn.py
@@ -1,3 +1,19 @@
 """Deep Q-Learning agents.
 """
+from collections import deque, namedtuple
 import torch
+
+
+# We use a `namedtuple` object here because it can
+# easily be used inside a PyTorch `Tensor` for vectorization.
+StateActionTransition = namedtuple(
+    'StateActionTransition',
+    [
+        'state',
+        'action',
+        'next_state',
+        'reward',
+    ],
+)
+
+
