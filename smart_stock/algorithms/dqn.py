0a1bddf5a401aa5e06749b9404016508a7ddc53c
Alexander DeRieux <alexander.derieux@gmail.com>
2021-11-26 12:58:27 -0500

    New state-action replay memory class

--- smart_stock/algorithms/dqn.py
+++ smart_stock/algorithms/dqn.py
@@ -2,6 +2,7 @@
 """
 from collections import deque, namedtuple
 import torch
+import random
 
 
 # We use a `namedtuple` object here because it can
@@ -17,3 +18,42 @@ StateActionTransition = namedtuple(
 )
 
 
+class ReplayMemory(list):
+    """State-action transition replay memory.
+
+    This class stores a collection of state-action transitions
+    for use with RL agents when traversing environments.
+    """
+    def __init__(self, capacity: int):
+        self.memory = deque([], maxlen=capacity)
+
+    def __len__(self):
+        return len(self.memory)
+
+    def __getitem__(self, index: int):
+        return list(self.memory)[index]
+
+    def __setitem__(self, index: int, newvalue: object):
+        self.memory[index] = newvalue
+
+    def __delitem__(self, index: int):
+        del self.memory[index]
+
+    def __iter__(self):
+        for item in self.memory:
+            yield item
+
+    def __str__(self):
+        return str(list(self.memory))
+
+    def push(self, *args, **kwargs):
+        """Preserve a state-action transition in memory."""
+        self.memory.append(StateActionTransition(*args, **kwargs))
+
+    def pop(self):
+        """Retrieve last state-action transition from memory."""
+        return self.memory.pop()
+
+    def sample(self, size: int):
+        """Generate a random sampling without replacement from replay memory."""
+        return random.sample(self.memory, size)
