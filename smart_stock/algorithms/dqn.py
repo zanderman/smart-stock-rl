0d5d28f678323eef1fb1d405ccf94a7f911261f7
Alexander DeRieux <alexander.derieux@gmail.com>
2021-11-26 17:56:04 -0500

    Changed network class names

--- smart_stock/algorithms/dqn.py
+++ smart_stock/algorithms/dqn.py
@@ -62,7 +62,7 @@ class ReplayMemory(list):
         return random.sample(self.memory, size)
 
 
-class FFLinearBlock(torch.nn.Module):
+class FeedForwardLinearBlock(torch.nn.Module):
     """Simple feed-forward linear block layer with batchnorm and PReLU activation."""
     def __init__(self, input_dim: int, output_dim: int):
         super().__init__()
@@ -78,7 +78,7 @@ class FFLinearBlock(torch.nn.Module):
 
 # Example: https://gist.github.com/kkweon/52ea1e118101eb574b2a83b933851379
 # Example: https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
-class DQN_FFLinear(torch.nn.Module):
+class FeedForwardLinear(torch.nn.Module):
     """Deep Q-learning linear feed-forward network."""
     def __init__(self, dims: list[int]):
         super().__init__()
@@ -93,7 +93,7 @@ class DQN_FFLinear(torch.nn.Module):
         # The input/output dimensions are collected by
         # zipping the original list with a shift-by-1 version.
         for input_dim, output_dim in zip(dims, dims[1:]):
-            self.layers.append(FFLinearBlock(input_dim, output_dim))
+            self.layers.append(FeedForwardLinearBlock(input_dim, output_dim))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
