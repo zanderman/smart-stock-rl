d6912fe53f1b55daace63e175c329ede610fd2b0
Alexander DeRieux <alexander.derieux@gmail.com>
2021-11-27 18:35:16 -0500

    DQN now updates target policy network at regular intervals

--- smart_stock/algorithms/deepq/dqn.py
+++ smart_stock/algorithms/deepq/dqn.py
@@ -115,13 +115,13 @@ class DQN:
         batch_action_idxs = torch.Tensor([self.policy.action2index(action) for action in batch_actions.numpy()]).to(device=self.policy.device, dtype=int).unsqueeze(1)
 
         # Compute Q-values for each state in the batch.
-        q_values = self.policy.policy_net(batch_states).gather(1, batch_action_idxs)
+        q_values: torch.Tensor = self.policy.policy_net(batch_states).gather(1, batch_action_idxs)
 
         # Compute next-state Q-values.
-        next_q_values = self.target_policy_network(batch_next_states).max(1)[0].detach().to(device=self.policy.device)
+        next_q_values: torch.Tensor = self.target_policy_network(batch_next_states).max(1)[0].detach().to(device=self.policy.device)
 
         # Compute expected Q-values.
-        expected_q_values = batch_rewards + (next_q_values * self.gamma)
+        expected_q_values: torch.Tensor = batch_rewards + (next_q_values * self.gamma)
 
         # print('optimize_policy')
         # print('q_values.shape',q_values.shape)
@@ -133,7 +133,7 @@ class DQN:
         # print('batch_next_states.shape',batch_next_states.shape)
 
         # Compute loss.
-        loss = self.criterion(q_values, expected_q_values)
+        loss = self.criterion(q_values, expected_q_values.unsqueeze(1))
         # print('loss', loss)
         # print()
 
@@ -144,6 +144,7 @@ class DQN:
 
     def run_episode(self, 
         max_steps: int = None, 
+        target_update_freq: int = 10,
         render: bool = False, 
         render_mode: str = None,
     ):
@@ -180,6 +181,11 @@ class DQN:
 
             # Optimize the policy at the current step.
             self.optimize_policy()
+
+            # Update target network if necessary.
+            if (step % target_update_freq) == 0:
+                # print(f"[{step}] target update")
+                self.target_policy_network.load_state_dict(self.policy.policy_net.state_dict())
 
             # Accumulate rewards for the current episode.
             total_reward += reward.item()
