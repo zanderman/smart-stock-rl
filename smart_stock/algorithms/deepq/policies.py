6382ce5dc81e5832966eeb152a12e5928ee6a5b5
Alexander DeRieux <alexander.derieux@gmail.com>
2021-11-27 14:40:27 -0500

    WIP DQN policies

--- smart_stock/algorithms/deepq/policies.py
+++ smart_stock/algorithms/deepq/policies.py
@@ -21,14 +21,18 @@ class DQNPolicy(EpsilonGreedyPolicy, ContinuousStateDiscreteActionPolicy):
     def select_random_action(self, obs: torch.Tensor) -> torch.Tensor:
         """Return a randomized action from the action space."""
         # TODO stack actions for multiple states.
-        raise NotImplementedError
+        # raise NotImplementedError
 
-        # # Convert action to PyTorch Tensor.
+        # Convert action to PyTorch Tensor.
         # action = torch.Tensor([action], dtype=action.dtype, device=self.device)
 
-        # action = super().select_random_action(obs)
-        # action = np.array(action).flatten()[0] # Handle random sampling from 1D Box space.
-        # return action
+        action = super().select_random_action(obs)
+        action = np.array(action).flatten()[0] # Handle random sampling from 1D Box space.
+
+        # Convert action to PyTorch Tensor.
+        action = torch.Tensor([[action]], dtype=action.dtype, device=self.device)
+
+        return action
 
     def select_greedy_action(self, obs: torch.Tensor) -> torch.Tensor:
         """Return a greedy action from the action space based on the given observation."""
@@ -73,6 +77,22 @@ class FeedForwardLinearPolicy(DQNPolicy):
         # Create network.
         self.policy_net = FeedForwardLinear(dims)
 
+    @staticmethod
+    def state2tensor(state: np.ndarray) -> torch.Tensor:
+        """Helper to convert raw observation into PyTorch Tensor."""
+        t = torch.from_numpy(state) # Convert to tensor.
+        t = t.unsqueeze(0) # Add batch dimension.
+        t = t.float() # Convert to float.
+        return t
+
+    def select_greedy_action(self, obs: torch.Tensor) -> torch.Tensor:
+        """Return a greedy action from the action space based on the given observation."""
+        print('greedy action')
+        with torch.no_grad():
+            print('obs.shape',obs.shape,obs.dtype)
+            action = self.policy_net(obs)
+            print(action.shape)
+            return action
 
     def q_value(self, obs: torch.Tensor, action: int = None) -> torch.Tensor:
         """Compute Q-value for given observation and action.
@@ -92,8 +112,14 @@ class FeedForwardLinearPolicy(DQNPolicy):
         alpha: float
     ) -> tuple[np.ndarray, np.ndarray, float, bool]:
 
+        # Convert state to tensor.
+        curr_state = self.state2tensor(curr_state)
+        print('curr_state.shape',curr_state.shape,curr_state.dtype)
+
         # Select action according to policy.
         action = self.select_action(curr_state)
 
-        next_state, reward, done = None, None, None
+        # Take selected action and get information from environment.
+        next_state, reward, done, _ = env.step(action)
+
         return action, next_state, reward, done
