a017cf7b3e6767bbf4d1ae8fbdf7d1980ef95bce
Alexander DeRieux <alexander.derieux@gmail.com>
2021-11-27 13:07:30 -0500

    WIP DQN is now epsilon-greedy

--- smart_stock/algorithms/deepq/policies.py
+++ smart_stock/algorithms/deepq/policies.py
@@ -2,21 +2,36 @@ from __future__ import annotations
 import gym
 import numpy as np
 import torch
-from ..basepolicy import ContinuousStateDiscreteActionPolicy
+from ..basepolicy import ContinuousStateDiscreteActionPolicy, EpsilonGreedyPolicy
 
 
-class DQNPolicy(ContinuousStateDiscreteActionPolicy):
+class DQNPolicy(ContinuousStateDiscreteActionPolicy, EpsilonGreedyPolicy):
     def __init__(self,
         action_space: gym.Space,
         observation_space: gym.Space,
         epsilon: float,
         device: torch.device,
     ):
-        super().__init__(action_space, observation_space)
+        super().__init__(action_space, observation_space, epsilon)
 
         # Preserve DQN-specific members.
-        self.epsilon = epsilon
         self.device = device
+
+    def select_random_action(self, obs: torch.Tensor) -> torch.Tensor:
+        """Return a randomized action from the action space."""
+        # TODO stack actions for multiple states.
+        raise NotImplementedError
+
+        # # Convert action to PyTorch Tensor.
+        # action = torch.Tensor([action], dtype=action.dtype, device=self.device)
+
+        # action = super().select_random_action(obs)
+        # action = np.array(action).flatten()[0] # Handle random sampling from 1D Box space.
+        # return action
+
+    def select_greedy_action(self, obs: torch.Tensor) -> torch.Tensor:
+        """Return a greedy action from the action space based on the given observation."""
+        raise NotImplementedError
 
     def action_probs(self, obs: torch.Tensor) -> torch.Tensor:
         """Compute probabilities for each action using the given observation."""
@@ -29,20 +44,13 @@ class DQNPolicy(ContinuousStateDiscreteActionPolicy):
         alpha: float
     ) -> tuple[np.ndarray, float, bool]:
 
-        # Random action.
-        if np.random.uniform(0, 1) < self.epsilon:
-            action = self.action_space.sample()
-            action = np.array(action).flatten()[0] # Handle random sampling from 1D Box space.
-
-            # Convert action to PyTorch Tensor.
-            action = torch.Tensor([action], dtype=action.dtype, device=self.device)
-
-        # Epsilon-greedy action selection.
-        else:
-            # TODO get action from network.
-            pass
+        # Select action according to policy.
+        action = self.select_action(curr_state)
 
         # Take selected action and get information from environment.
         next_state, reward, done, _ = env.step(action)
+
+        # TODO Update policy.
+        pass
 
         return next_state, reward, done
